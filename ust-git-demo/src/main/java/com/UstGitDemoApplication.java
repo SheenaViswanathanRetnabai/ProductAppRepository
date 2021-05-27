ef3c8e4ecea6785ab336665159a6753a49aa2485
ustjava06 <ustjava06@java17.iiht.tech>
2021-05-27 14:15:25 +0530

    commited

---
+++ ust-git-demo/src/main/java/com/UstGitDemoApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UstGitDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UstGitDemoApplication.class, args);
+	}
+
+}
